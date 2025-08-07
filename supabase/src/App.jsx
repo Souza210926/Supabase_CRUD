560967c2f738c271df4abc50cc8a29ccab9f0205
Souza210926 <154094036+Souza210926@users.noreply.github.com>
2025-08-07 09:13:04 -0300

    Primeiro commit de criação

---
+++ supabase/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div> 
+          <h1>Sistema de Login</h1>
+          <input type="text" placeholder='Name' /><br />
+          <input type="text" placeholder='email'/><br />
+          <input type="number" placeholder='age'/><br />
+
+          <button>Enviar</button>
+      </div>
+    </>
+  )
+}
+
+export default App
